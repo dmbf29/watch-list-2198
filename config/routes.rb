aa4a10a4560df9581cba1163a5f4640f80a79a8d
Douglas Berkley <douglasmberkley@gmail.com>
2026-02-19 18:13:11 +0900

    watchlist livecode

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # [X] - As a user, I can see all my lists
+  # [X] - As a user, I can create a list
+  # [X] - As a user, I can see the details of a list
+  # [ ] - As a user, I can bookmark a movie inside a list
+  # [ ] - As a user, I can destroy a bookmark
+  resources :lists, only: [ :index, :show, :new, :create ] do
+    resources :bookmarks, only: [ :new, :create ]
+  end
 end
