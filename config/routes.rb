d23f3fbd190c2c78165094a267ebca31e01a119c
Douglas Berkley <douglasmberkley@gmail.com>
2026-02-20 09:28:32 +0900

    added root path

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # [X] - As a user, I can see the details of a list
   # [ ] - As a user, I can bookmark a movie inside a list
   # [ ] - As a user, I can destroy a bookmark
+  root to: "lists#index"
   resources :lists, only: [ :index, :show, :new, :create ] do
     resources :bookmarks, only: [ :new, :create ]
   end
